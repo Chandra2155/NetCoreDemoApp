8fab3481674cd63d0fc4986d4510e898446a9ef7
Chandra2155 <55084453+Chandra2155@users.noreply.github.com>
2021-07-05 23:19:02 +0530

    Added comments

--- NetCoreDemoApp/Controllers/HomeController.cs
+++ NetCoreDemoApp/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace NetCoreDemoApp.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            //addded comments in Master branch
         }
 
         public IActionResult Index()
