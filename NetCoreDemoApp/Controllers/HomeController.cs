ca4bdd34f1f2d588dafb331ae733767eea532453
Chandra2155 <palugula23@gmail.com>
2021-07-05 22:57:01 +0530

    Update HomeController.cs

--- NetCoreDemoApp/Controllers/HomeController.cs
+++ NetCoreDemoApp/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace NetCoreDemoApp.Controllers
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+			//adderd comments
         }
 
         public IActionResult Index()
